63d89646df690a8c6e4814aebcf5a336b2dbf0a7
Chirag-debug <mulla.chirag@gmail.com>
2021-01-16 12:34:39 +0530

    Completed Sign In Modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
+/* Sign Up Modal */
 var modalSU = document.getElementById("signUpModal");
 var btnSU = document.getElementById("signUp");
-var closeSU = document.getElementsByClassName("closeSU")[0];
+var closeSU = document.getElementById("closeSU");
 
 btnSU.onclick = function() {
     modalSU.style.display = "block";
@@ -8,4 +9,24 @@ btnSU.onclick = function() {
 
 closeSU.onclick = function() {
     modalSU.style.display = "none";
+}
+
+/* Sign In Modal */
+var modalSI = document.getElementById("signInModal");
+var btnSI = document.getElementById("signIn");
+var closeSI = document.getElementById("closeSI");
+
+btnSI.onclick = function() {
+    modalSI.style.display = "block";
+}
+
+closeSI.onclick = function() {
+    modalSI.style.display = "none";
+}
+
+var SUlink = document.getElementById("linkSU");
+
+SUlink.onclick = function() {
+    modalSI.style.display = "none";
+    modalSU.style.display = "block";
 }
