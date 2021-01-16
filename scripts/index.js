87183656e1b7c8ad751cfab0812863f3bb514c68
Chirag-debug <mulla.chirag@gmail.com>
2021-01-16 14:32:36 +0530

    PartA 90% completed

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,20 @@ closeSI.onclick = function() {
 
 var SUlink = document.getElementById("linkSU");
 
+/* Sign Up Link*/
 SUlink.onclick = function() {
     modalSI.style.display = "none";
     modalSU.style.display = "block";
+}
+
+/* Create Post Modal */
+var modalCP = document.getElementById("createPostModal");
+var btnCP = document.getElementById("bodyBtn2");
+var closeCP = document.getElementById("closeCP");
+
+btnCP.onclick = function () {
+    modalCP.style.display = "block";
+}
+closeCP.onclick = function () {
+    modalCP.style.display = "none";
 }
