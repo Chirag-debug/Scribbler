94f17a0706666c9bbdf9e8969b27e8636a95faf8
Chirag-debug <mulla.chirag@gmail.com>
2021-01-17 14:22:57 +0530

    Changed Common.css & common.js & created header for postslist.html

---
+++ scripts/common.js
@@ -0,0 +1,33 @@
+/* Sign Up Modal */
+var modalSU = document.getElementById("signUpModal");
+var btnSU = document.getElementById("signUp");
+var closeSU = document.getElementById("closeSU");
+
+btnSU.onclick = function() {
+    modalSU.style.display = "block";
+}
+
+closeSU.onclick = function() {
+    modalSU.style.display = "none";
+}
+
+/* Sign In Modal */
+var modalSI = document.getElementById("signInModal");
+var btnSI = document.getElementById("signIn");
+var closeSI = document.getElementById("closeSI");
+
+btnSI.onclick = function() {
+    modalSI.style.display = "block";
+}
+
+closeSI.onclick = function() {
+    modalSI.style.display = "none";
+}
+
+var SUlink = document.getElementById("linkSU");
+
+/* Sign Up Link*/
+SUlink.onclick = function() {
+    modalSI.style.display = "none";
+    modalSU.style.display = "block";
+}
