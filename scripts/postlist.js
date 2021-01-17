d65abca134cbb0e50a6ef23abe60ea83628f1b24
Chirag-debug <mulla.chirag@gmail.com>
2021-01-18 01:28:35 +0530

    Minor changes in PartB

--- scripts/postlist.js
+++ scripts/postlist.js
@@ -3,7 +3,6 @@ function deleteCard(card) {
     var delModal = document.getElementById("delpost");
     var yesButton = document.getElementById("yesBtn");
     var noButton = document.getElementById("noBtn");
-    var closeDialog = document.getElementById("close-dialog");
     delModal.style.display = "block";
 
     yesButton.onclick = function() {
@@ -12,10 +11,6 @@ function deleteCard(card) {
     }
 
     noButton.onclick = function() {
-        delModal.style.display = "none";
-    }
-
-    closeDialog.onclick = function() {
         delModal.style.display = "none";
     }
 }
