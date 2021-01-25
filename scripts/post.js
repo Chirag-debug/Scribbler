2fe8de207339101066b2008f9ff637755c26ab03
Chirag-debug <mulla.chirag@gmail.com>
2021-01-25 22:57:18 +0530

    edit feature half way through

--- scripts/post.js
+++ scripts/post.js
@@ -8,18 +8,18 @@ function postDynamic() {
     let postDescription = localStorage.getItem('description');
     
     
-    var template = '<div class="title-container">'+
+    var template = '<div class="title-container" id="title-details">'+
     '<h4 id="post-title">'+
     postTitle+
     '</h4>'+
     '</div>'+
-    '<div class="author">'+
+    '<div class="author" id="author-details">'+
     '<h4 id="autr">'+
     postAuthor+
     '</h4>'+
     '</div>'+
-    '<div class="body-container">'+
-    '<p>'+
+    '<div class="body-container" id="body-details">'+
+    '<p id="desc">'+
     postDescription+
     '</p>'+
     '</div>';
@@ -46,7 +46,85 @@ function addComment(){
   }
 
   function editPost() {
-      
+
+      var edit = '<button id="editBtn" >Edit  <i class="fa fa-pencil-square-o" style="font-size:18px"></i> </button>';
+      var save = '<button id="editBtn" >Save  <i class="fa fa-save" style="font-size:18px"></i> </button>';
+
+      var editSave = document.getElementById('editPost');
+      var toDo = (editSave.innerText).toString().trim();
+      if(toDo.localeCompare('Edit')===0){
+        var btnSave = document.getElementById('editPost');
+        btnSave.innerHTML = save;
+        enableEdit();
+    
+      }
+      else{
+        var btnEdit = document.getElementById('editPost');
+        btnEdit.innerHTML = edit;
+        updateContent();
+      }
+  }
+
+  function enableEdit(){
+
+    var postTitle = (document.getElementById('post-title').innerHTML).toString();
+    var postAuthor = (document.getElementById('autr').innerHTML).toString();
+    var postDescription = (document.getElementById('desc').innerHTML).toString();
+
+    
+    var div = document.getElementById('post-details');
+    
+    var template = 
+    '<textarea class="txtTitle" id="txtTitleId">'+
+    postTitle+
+    '</textarea>'+
+    '<br>'+
+    '<br>'+
+
+    '<textarea class="txtAuthor" id="txtAuthorId">'+
+    postAuthor+
+    '</textarea>'+
+    '<br>'+
+    '<br>'+
+    '<br>'+
+
+    '<textarea class="txtBody" id="txtBodyId" rows="10" cols="150">'+
+    postDescription+
+    '</textarea>';
+    
+
+    div.innerHTML = template;
+  }
+
+  function updateContent(){
+
+    var editedTitle = (document.getElementById('txtTitleId').innerHTML).toString();
+    var editedAuthor = (document.getElementById('txtAuthorId').innerHTML).toString();
+    var editedDescription = (document.getElementById('txtBodyId').innerHTML).toString();
+
+    console.log(editedTitle);
+    console.log(editedAuthor);
+    console.log(editedDescription);
+  
+
+    var template = '<div class="title-container" id="title-details">'+
+    '<h4 id="post-title">'+
+    editedTitle+
+    '</h4>'+
+    '</div>'+
+    '<div class="author" id="author-details">'+
+    '<h4 id="autr">'+
+    editedAuthor+
+    '</h4>'+
+    '</div>'+
+    '<div class="body-container" id="body-details">'+
+    '<p id="desc">'+
+    editedDescription+
+    '</p>'+
+    '</div>';
+
+    document.getElementById("post-details").innerHTML = template;
+  
   }
 
 
